a5879b9f1202c8d322ac91810098dc2672b720ba
JuanEstradaV <juanjo.jjev@gmail.com>
2023-11-26 22:59:53 -0500

    http interceptor token, guards pages

--- InsideFront/src/app/app.config.ts
+++ InsideFront/src/app/app.config.ts
@@ -1,8 +1,15 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { HttpClientModule, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
 
 import { routes } from './app.routes';
+import { addTokenInterceptor } from './utils/add-token.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(HttpClientModule),
+    provideHttpClient(withInterceptors([addTokenInterceptor])),
+  ]
 };
---
+++ InsideFront/src/app/guards/login.guard.ts
@@ -0,0 +1,34 @@
+import {inject} from '@angular/core'
+import { Router } from '@angular/router'
+
+export const loginGuard = ():boolean => {
+    const router = inject(Router)
+    if (localStorage.getItem('token')) {
+        return true
+    } else {
+        router.navigate([''])
+        return false
+    }
+}
+
+export const rolVigilante = () => {
+    const rol = localStorage.getItem('rol')
+    const router = inject(Router)
+    if(rol === 'Vigilante' || rol === 'Administrador') {
+        return true
+    } else {
+        router.navigate([''])
+        return false
+    }
+}
+
+export const rolAdmin = () => {
+    const rol = localStorage.getItem('rol')
+    const router = inject(Router)
+    if(rol === 'Administrador') {
+        return true
+    } else {
+        router.navigate([''])
+        return false
+    }
+}

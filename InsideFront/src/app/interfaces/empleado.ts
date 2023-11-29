dc5bf4bfc766d9b23dbad11cc1dcd11b523a79b7
Adams_GMR <juan_adams82212@elpoli.edu.co>
2023-11-29 01:19:36 -0500

    formulario empleado,formulario residente, interfaces

--- InsideFront/src/app/interfaces/empleado.ts
+++ InsideFront/src/app/interfaces/empleado.ts
@@ -1,15 +1,7 @@
 export interface Empleado {
-<<<<<<< HEAD
-    cedula: number,
-    rol:string,
-    observaciones:string,
-    direccion:string
-}
-=======
     nombre?: string,
     cedula: number,
     rol: string,
     observaciones: string,
     direccion: string
 }
->>>>>>> 204ca0b8be890ea2242f187e9dc1f0dcecdf8e3e

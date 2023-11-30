3d88bdb1d27b70b84ba7912372ef89e6d64a048e
JuanEstradaV <juanjo.jjev@gmail.com>
2023-11-30 12:34:43 -0500

    errores regex validators y formularios agregar Persona

--- InsideFront/src/app/components/formulario-residente/formulario-residente.component.ts
+++ InsideFront/src/app/components/formulario-residente/formulario-residente.component.ts
@@ -25,7 +25,7 @@ export class FormularioResidenteComponent {
   textRegex = /^[a-zA-ZáéíóúüñÁÉÍÓÚÜÑ\s]+$/
   mailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
   passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
-  cedRegex = /^[0-9]\d{7,9}$/
+  cedRegex = /^[0-9]\d{7,10}$/
   telRegex = /^\d{10}$/
 
   form =  new FormGroup({
